aa3f7d792f574da7e73601e8a2a02f63a5cfd68d
totto2727 <kaihatu.totto2727@gmail.com>
2021-05-03 11:18:15 +0900

    備考モーダル化

--- src/BusTimetable.tsx
+++ src/BusTimetable.tsx
@@ -1,13 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import axios from "axios";
 import {DataGrid, GridColDef, GridValueFormatterParams} from "@material-ui/data-grid";
 import {
-
-    Button, Dialog, Paper,
-
-
+    Button, Dialog, Paper, Typography,
 } from "@material-ui/core";
-
 
 type Direction = "outward" | "homeward";
 
@@ -21,10 +17,10 @@ type TimetableSlot = {
 
 type TimetableItem = {
     id: number,
-    start: Date | undefined,
-    via1: Date | undefined,
-    via2: Date | undefined,
-    goal: Date | undefined,
+    start?: Date,
+    via1?: Date,
+    via2?: Date,
+    goal?: Date,
     remarks: string
 }
 
@@ -32,43 +28,37 @@ type GasResponse = {
     values: TimetableSlot[]
 }
 
-const valueFormatter = (params: GridValueFormatterParams) => {
-    const date = params.value as Date | undefined;
-    return date !== undefined ? `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}` : date;
-};
-
 const gasGetBusTimetable = "https://script.google.com/macros/s/AKfycbyLqwd2q-JoOBW3OIRH3oCMR0WhOKxICeBn9vMFQvRx2JE6J_TMxxyPhp6EEph6GFNA/exec";
 
 const Remarks: React.FC<{ remarks: string }> = props => {
     const [open, setOpen] = useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+    const handleClickOpen = useCallback(() => setOpen(true), [])
 
-    const handleClickClose = () => {
-        setOpen(false);
-    };
+    const handleClickClose = useCallback(() => setOpen(false), [])
 
     return (
         props.remarks !== ""
             ? <div>
-                <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                <Button variant={"contained"} color="primary" onClick={handleClickOpen}>
                     備考
                 </Button>
                 <Dialog open={open} onClose={handleClickClose}>
                     <Paper style={{
                         whiteSpace: "pre-wrap",
-                        width: "auto",
-                        height: "auto",
-                        fontSize: "2rem",
                         minWidth: 300,
                         minHeight: 300,
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        lineHeight:"2em"
-                    }}>{props.remarks}</Paper>
+                    }}>
+                        <Typography style={{
+                            padding: "2rem",
+                            fontSize: "2rem", display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            lineHeight: "2em"
+                        }}>
+                            {props.remarks}
+                        </Typography>
+                    </Paper>
                 </Dialog>
             </div>
 
@@ -76,51 +66,51 @@ const Remarks: React.FC<{ remarks: string }> = props => {
     )
 }
 
-const columns: GridColDef[] = [
-    {headerName: "ID", field: "id", hide: true, type: "number"},
-    {headerName: "発車", field: "start", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
-    {headerName: "経由1", field: "via1", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
-    {headerName: "経由2", field: "via2", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
-    {headerName: "終点", field: "goal", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
-    {
-        headerName: "備考",
-        field: "remarks",
-        flex: 15,
-        renderCell: params => <Remarks remarks={params.value as string}/>
-    }
-]
+const columns: GridColDef[] = (() => {
+    const valueFormatter = (params: GridValueFormatterParams) => {
+        const date = params.value as Date | undefined;
+        return date !== undefined ? `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}` : date;
+    };
 
-const stringToTime = (str: string) => str !== "" ? new Date(str) : undefined
-
-const makeTimetableItemList: (slot: TimetableSlot[]) => TimetableItem[] = slot =>
-    slot.map((v, id) => {
-        const {start, via1, via2, goal, ...data} = v;
-        return {
-            ...data,
-            id,
-            start: stringToTime(start),
-            via1: stringToTime(via1),
-            via2: stringToTime(via2),
-            goal: stringToTime(goal),
+    const columns: GridColDef[] = [
+        {headerName: "ID", field: "id", hide: true, type: "number"},
+        {headerName: "発車", field: "start", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
+        {headerName: "経由1", field: "via1", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
+        {headerName: "経由2", field: "via2", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
+        {headerName: "終点", field: "goal", flex: 2, valueFormatter, align: "center", headerAlign: "center"},
+        {
+            headerName: "備考",
+            field: "remarks",
+            flex: 15,
+            renderCell: params => <Remarks remarks={params.value as string}/>
         }
-    })
+    ]
+
+    return columns;
+})();
 
 export const BusTimetable: React.FC<{ direction: Direction, title: string }> = props => {
     const [timetable, setTimetable] = useState<TimetableItem[]>([]);
 
+    const makeTimetableItemList = useCallback((slot: TimetableSlot[]): TimetableItem[] => {
+        const stringToTime = (str: string) => str !== "" ? new Date(str) : undefined
+        return slot.map((v, id) => {
+            const {remarks, ...rest} = v;
+            return Object.assign({remarks, id}, ...Object.entries(rest).map(v => ({[v[0]]: stringToTime(v[1])})));
+        })
+    }, [])
+
     useEffect(() => {
         axios
             .get<GasResponse>(`${gasGetBusTimetable}?direction=${props.direction}`)
-            .then(res => {
-                setTimetable(makeTimetableItemList(res.data.values));
-                console.log(JSON.stringify(res.data));
-            });
-    }, []);
+            .then(res => setTimetable(makeTimetableItemList(res.data.values)))
+            .catch(()=>alert("データを取得できません。\n再読込してください。"));
+    }, [makeTimetableItemList, props.direction]);
 
     return (
         <div>
             <h2>{props.title}</h2>
             <DataGrid rows={timetable} columns={columns} autoPageSize autoHeight/>
         </div>
-    )
+    );
 }
--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,10 +1,10 @@
 import {createMuiTheme} from "@material-ui/core";
-import {green, purple} from "@material-ui/core/colors";
+import {blue, green} from "@material-ui/core/colors";
 
 export const theme=createMuiTheme({
     palette: {
         primary: {
-            main: purple[500],
+            main: blue[500],
         },
         secondary: {
             main: green[500],
